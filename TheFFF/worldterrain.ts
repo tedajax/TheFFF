7eaeb0ac21ff1cc049e97277c33a87713c0f3e9f
Ted Dobyns <ted@tedajax.net>
2013-12-15 19:24:35 -0800

    use zacs terrain

--- TheFFF/worldterrain.ts
+++ TheFFF/worldterrain.ts
@@ -26,16 +26,16 @@ class WorldTerrain {
         this.textureMapping[7] = "water0";
         this.textureMapping[8] = "water1";
 
-        this.worldDescriptor = this.genMap(this.width, this.height);
-        //var data = game.config.world_data;
-        //this.worldDescriptor = [];
-        //var index = 0;
-        //for (var i = 0; i < this.width; ++i) {
-        //    this.worldDescriptor[i] = [];
-        //    for (var j = 0; j < this.height; ++j) {
-        //        this.worldDescriptor[i][j] = data[index++];
-        //    }
-        //}        
+        //this.worldDescriptor = this.genMap(this.width, this.height);
+        var data = game.config.world_data;
+        this.worldDescriptor = [];
+        var index = 0;
+        for (var i = 0; i < this.width; ++i) {
+            this.worldDescriptor[i] = [];
+            for (var j = 0; j < this.height; ++j) {
+                this.worldDescriptor[i][j] = data[index++];
+            }
+        }        
 
         this.tileCountX = Math.ceil(game.canvas.width / this.tileWidth) + 2;
         this.tileCountY = Math.ceil(game.canvas.height / this.tileHeight) + 1;
